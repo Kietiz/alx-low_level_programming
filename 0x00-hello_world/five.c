f3495b9bc5480cf362bdf31202c7122120bdfeac
Kietiz <alexkiety65@gmail.com>
2022-09-09 09:47:32 -0700

    c

---
+++ 0x00-hello_world/five.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - prints a string using printf
+ * description - program prints a string and a new line.
+ * return: int (0) when successfull and no errors
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
