73ac915a3e320dea6bb8e47e5ce5e2f4f766a6a7
Kietiz <alexkiety65@gmail.com>
2022-09-09 09:26:23 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * 
+ * main - prints a string using puts function
+ * description - this program prints strings followed by new line 
+ * return: int 0 when done and there are no errors
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
