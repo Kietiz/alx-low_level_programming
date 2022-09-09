c430a8884cfd8024f455ee00f4657226fa07c29d
Kietiz <alexkiety65@gmail.com>
2022-09-09 10:36:31 -0700

    c; ;

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
-/*
- * main - prints a string using printf
- * description - program prints a string and a new line.
- * return: int (0) when successfull and no errors
+#include <unistd.h>
+/**
+ * main - prints a string using print
+ * Description: Prints a string of characters when successful
+ * Return: int (0) when successfull and no errors
  */
-
 int main(void)
 {
 	printf("\"with proper grammar, but the outcome is a piece of art");
 	return (0);
+}
