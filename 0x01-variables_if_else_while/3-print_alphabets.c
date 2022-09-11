b2671cfb74c508ec4063e0e32fcc635f6c22f9bc
Kietiz <alexkiety65@gmail.com>
2022-09-11 12:42:58 -0700

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints out alphabets in small and caps
+ * Description: uses for loop and putchar
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		putchar(x);
+	putchar(toupper(x);
+			putchar('\n');
+			return (0);
+}
+
