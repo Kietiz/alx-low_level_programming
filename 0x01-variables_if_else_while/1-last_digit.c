0a7d8b5bf42321f0fdb7635970b4313ada98986c
Kietiz <alexkiety65@gmail.com>
2022-09-11 07:03:51 -0700

    c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - looks at the last value of a number and describes it
+ * description: uses srand and if else
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+		ln = n % 10;
+	if (ln > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, ln);
+	else if (ln == 0)
+		printf("Last digit of %d is %d and is 0\n", n, ln);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ln);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - looks at the last value of a number and describes it
+ * description: uses srand and if else
+ * Return: return 0 if successful
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					/* your code goes there */
+
+					return (0);
+
+}
