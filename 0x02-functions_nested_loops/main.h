68ee3b28e6757a64a3dc6e243bd6204db69204cc
Kietiz <alexkiety65@gmail.com>
2022-09-14 14:54:58 -0700

    c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - print aphabets
- * Description: prints a to z in small leters
- * Return: always 0
- */
-
-int print_alphabet()
-{
-	int x;
-
-	for (x = 'a' ; x <= 'z'; x++)
-		putchar(x);
-	putchar('\n');
-	return (x);
-}
