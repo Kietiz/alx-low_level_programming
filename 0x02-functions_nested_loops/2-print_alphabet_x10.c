10a9014cd5e61ec59d585539178f0ae23c33d3c7
Kietiz <alexkiety65@gmail.com>
2022-09-14 17:59:51 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,10 @@
 
 void print_alphabet_x10(void)
 {
+	int i;
 	char c;
-	int i = 0;
 
 	for (c = 'a' ; c <= 'z' ; c++)
-		while (i < 10)
+		for (i = 0 ; i < 10 ; i++)
 			_putchar(c);
-	_putchar('\n');
-	i++;
-}
+	putchar('\n');
