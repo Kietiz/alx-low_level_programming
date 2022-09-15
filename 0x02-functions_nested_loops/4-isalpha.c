b5f7d57bfd901bfe14536db6f5f4d5899a35c09e
Kietiz <alexkiety65@gmail.com>
2022-09-14 18:52:49 -0700

    c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * _isalpha - returns caps and small letters
+ * @c: program input
+ * Return: retun 1 if c  is a letter and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
