d1f084db8f8f671b5c89d2a563f4098f3a706095
Kietiz <alexkiety65@gmail.com>
2022-09-15 13:47:22 -0700

    c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-		printf("98");
+		printf(98);
 		_putchar('\n');
 	}
 	else if (n == 98)
@@ -28,7 +28,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-		printf("98");
+		printf(98);
 		_putchar('\n');
 	}
 }
