2b3f8180df3f01b52bc91377c958959371d804a9
Kietiz <alexkiety65@gmail.com>
2022-09-15 13:44:00 -0700

    c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,11 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n ; n <= 98 ; n++)
+		for (n = n ; n < 98 ; n++)
 		{
 			printf("%d, ", n);
 		}
+		printf("98");
 		_putchar('\n');
 	}
 	else if (n == 98)
@@ -27,6 +28,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
+		printf("98");
 		_putchar('\n');
 	}
 }
