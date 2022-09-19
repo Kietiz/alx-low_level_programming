6afbd96e87c4edcb06adf5e2b581329a410f8dff
Kietiz <alexkiety65@gmail.com>
2022-09-19 16:55:03 -0700

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a stirng in reverse
+ * @s: string
+ * Return: always 0
+ */
+
+void print_rev(char *s)
+{
+	int a;
+	int b;
+	int fc;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+		fc++;
+	}
+	for (b = (fc - 1) ; b >= 0 ; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
