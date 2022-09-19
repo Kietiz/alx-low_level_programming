6d42d4dd6a6cc02a3f0d9774c155382fe1e8d332
Kietiz <alexkiety65@gmail.com>
2022-09-19 16:16:52 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - retururns the length of a sring
+ * @s: the string
+ * @*s: address of the string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = sizeof(s);
+	return (n);
+}
