2e50a7bb479fbec9967d9d495de523e4222ed25b
Kietiz <alexkiety65@gmail.com>
2022-09-19 18:23:54 -0700

    c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print every third number
+ * @str: the string
+ * Return: null
+ */
+
+void puts2(char *str)
+{
+	int a;
+	int b;
+	int count = 0;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+		count++;
+	for (b = 0 ; b <= count ; b = b + 2)
+	{
+		_putchar(s[b] + '0');
+	}
+	_putchar('\n');
+}
