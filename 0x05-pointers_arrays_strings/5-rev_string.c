1afb8a4b87cb8177ec1dab1a8f30170d44e2c09a
Kietiz <alexkiety65@gmail.com>
2022-09-19 17:03:58 -0700

    c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - program reverse a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int a;
+	int b;
+	int fc = 0;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+		fc++;
+	}
+	for (b = (fc - 1) ; b >= 0 ; b--)
+	{
+		return (s[b]);
+	}
+}
