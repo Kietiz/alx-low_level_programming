4c9ae67260bbb6be953c66eca9e819287cdbf39e
Kietiz <alexkiety65@gmail.com>
2022-09-19 18:37:11 -0700

    c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,8 +14,9 @@ void puts2(char *str)
 
 	for (a = 0 ; str[a] != '\0' ; a++)
 		count++;
-	for (b = 0 ; b <= count ; b = b + 2)
+	for (b = 0 ; b < count ; b = b + 2)
 	{
 		_putchar(str[b]);
 	}
+	_putchar('\n');
 }
