2b006b81110e7cbe0bed68c2c2864c67dd506410
Kietiz <alexkiety65@gmail.com>
2022-09-22 17:47:04 -0700

    c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,9 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0 ; src[i] != '\0' ; i++)
 	{
-	}
-
-	for (n = 0 ; (dest[i + n] = *src++) != '\0' ; n++)
-	{
+		for (n = 0 ; (dest[i + n] = *src++) != '\0' ; n++)
+		{
+		}
 	}
 
 	return (dest);
