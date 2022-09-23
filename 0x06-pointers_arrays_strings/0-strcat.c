fc191e6722f217d6a4d5e74f0aa50f7f4ea1d34d
Kietiz <alexkiety65@gmail.com>
2022-09-22 17:30:10 -0700

    c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: paste destination
+ * @src: copy source
+ * Return: return dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int n;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+	}
+	for (n = 0 ; dest[i + n] != '\0' ; n++)
+	{
+	}
+	return (dest);
+}
