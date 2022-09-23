16b442ccbb61361d217fcef756600d0802f1ad80
Kietiz <alexkiety65@gmail.com>
2022-09-22 18:23:43 -0700

    c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,6 +16,8 @@ char *_strncpy(char *dest, char *src, int n)
 	while (dest[i++])
 		j++;
 	for (i = 0 ; dest[i++] && i < n ; i++)
-		dest[j] = src[i];
+		dest[i] = src[i];
+	for (i = j ; i < n ; i++)
+		dest[index] = '\0';
 	return (dest);
 }
